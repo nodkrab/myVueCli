dacecbc0c28f1b28285011649c91a2cdf8503dd0
Karit <nodkrab@gmail.com>
2019-05-22 16:20:55 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter";
+import UserList from "./components/UserList";
+import AppBar from "./components/AppBar";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    counter: Counter,
+    "user-list": UserList,
+    appbar: AppBar
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+    <div>
+        <section class="section">
+            <div class="container">
+                <!-- <counter/> -->
+                <!-- <user-list/> -->
+                <appbar/>
+                <router-view/>
+            </div>
+        </section>
+    </div>
+</template>
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+import "bulma/css/bulma.min.css";
+import routes from "./routes";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+const router = new VueRouter({
+  routes: routes,
+  mode: "history"
+});
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  router: router
+}).$mount("#app");
